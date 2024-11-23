2a8b31773d7a4f2a05bd1b5e5411ddbd54053c3f
Harsh Galiyawala <109781796+harshGALIYAWALA@users.noreply.github.com>
2024-11-23 13:39:22 +0530

    connected fragments

--- android/app/src/main/java/com/example/myapplication/activities/activities/MainActivity.java
+++ android/app/src/main/java/com/example/myapplication/activities/activities/MainActivity.java
@@ -1,26 +1,76 @@
 package com.example.myapplication.activities.activities;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.myapplication.R;
+import com.example.myapplication.activities.fragments.notes_fragment;
+import com.example.myapplication.activities.fragments.todo_fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.mancj.materialsearchbar.MaterialSearchBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView btnNavView;
+    FrameLayout frame_layout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        btnNavView = findViewById(R.id.btnNavView);
+        frame_layout = findViewById(R.id.frame_layout);
+
+        // bottomNavView
+        btnNavView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int id = item.getItemId();
+
+                if(id == R.id.menu_note) {
+                    loadFrag(new notes_fragment(), false);
+                } else {
+                    loadFrag(new todo_fragment(), true);
+                }
+
+                return true;
+            }
+        });
+
+        btnNavView.setSelectedItemId(R.id.menu_note);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    public void loadFrag(Fragment fragment, boolean flag) {
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        if(flag){
+            ft.add(R.id.frame_layout, fragment);
+        } else {
+            ft.replace(R.id.frame_layout, fragment);
+        }
+        ft.commit();
+
+    }
+
 }
